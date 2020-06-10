043cd85bad89444684acc5dba27715f4cf21d430
Reinaldo Salla <reinaldo.salla@compasso.com.br>
2020-06-10 17:27:06 -0300

    Apply translateX with visibility

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,15 +29,9 @@ import React, {
 import throttle from "lodash.throttle";
 import "./App.css";
 
-let nCalls = 0;
-let useVisibilityNumCalls = 0;
-
-const useVisibility = (throttleMiliseconds=100) => {
+const useVisibility = () => {
 	const [isVisible, setIsVisible] = useState(false);
 	const currentElement = createRef(null);
-
-	useVisibilityNumCalls++;
-	console.log(`useVisibilityNumCalls = ${useVisibilityNumCalls}`);
 
 	// onScroll is executed only once every 100 ms.
 	const onScroll = throttle(() => {
@@ -50,7 +44,7 @@ const useVisibility = (throttleMiliseconds=100) => {
 			top >= 0 && 
 			top <= window.innerHeight
 		);
-	}, throttleMiliseconds);
+	}, 1);
 
 	useEffect(() => {
 		window.addEventListener("scroll", onScroll);
@@ -60,57 +54,20 @@ const useVisibility = (throttleMiliseconds=100) => {
 	return [isVisible, currentElement];
 };
 
-/*
-const useVisibility = () => {
-	const [debounce, setDebounce] = useState(0);
-	const [isVisible, setIsVisible] = useState(false);
-	const currentElement = createRef(null);
-
-	useVisibilityNumCalls++;
-	console.log(`useViibilityNumCalls = ${useVisibilityNumCalls}`);
-	console.log(`debounce = ${debounce}`);
-
-	const onScroll = () => {
-		if (!currentElement.current) {
-			setIsVisible(false);
-			return
-		}
-		const top = currentElement.current.getBoundingClientRect().top;
-		setIsVisible(top >= 0 && top <= window.innerHeight);
-	}
-
-	useEffect(() => {
-		const intervalId = setInterval(() => {
-			setDebounce(debounce + 1);
-		}, 5000);
-		return () => clearInterval(intervalId);
-	}, [debounce]);
-
-	useEffect(() => {
-		window.addEventListener("scroll", onScroll);
-		return () => window.removeEventListener("scroll", onScroll);
-	}, [debounce]);
-
-	return [isVisible, currentElement];
-};
-*/
-
 const App = () => {
 	const [isFirstVisible, firstRef] = useVisibility();
 	const [isSecondVisible, secondRef] = useVisibility();
+	const firstClassName = isFirstVisible 
+		? "first-element-on"
+		: "first-element-off";
 
 	useEffect(() => {
 		document.title = `${isFirstVisible} - ${isSecondVisible}`
 	}, [isFirstVisible, isSecondVisible]);
 
-	nCalls++;
-	console.log(`call nº ${nCalls}`)
-	console.log(`isFirstVisisble = ${isFirstVisible}`);
-	console.log(`isSecondVisible = ${isSecondVisible}`);
-
 	return (
 		<div className="app">
-			<div ref={firstRef} className="first-element">
+			<div ref={firstRef} className={firstClassName}>
 				Event isFirstVisible
 			</div>
 			<div ref={secondRef} className="second-element">
