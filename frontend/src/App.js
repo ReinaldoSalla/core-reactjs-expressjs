269c2028986fb0a601925062ebcdb96b55dbbcaf
Reinaldo Salla <reinaldo.salla@compasso.com.br>
2020-06-18 14:41:30 -0300

    Creating skeleton for useVisibility several items

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+/*
 import React, { Fragment } from "react";
 import Topbar from "./components/Topbar";
 import Courosel from "./components/Courosel";
@@ -17,6 +18,47 @@ const App = () => {
       <Footer />
 		</Fragment>
 	);
+};
+
+export default App;
+*/
+
+// useVisibilityTimeOnceDown
+
+import React from "react";
+import "./App.css";
+
+/*
+const data = new Array(12).fill(0).map((_, index) => 
+  `Content ${index}`
+);
+*/
+
+const data = new Array(12).fill(0).map((_, index) => ({
+  text: `Content ${index + 1}`
+}));
+
+const Item = ({ text }) => (
+  <div className="item">
+    {text}
+  </div>
+);
+
+const Items = () => {
+  const listItems = data.map((item, index) => 
+    <Item key={index} text={item.text}/>
+  );
+  return (
+    <div className="items">
+      {listItems}
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Items />
+  );
 };
 
 export default App;
