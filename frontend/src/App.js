33e5730bbddc8107b89e4ab8c70dcc90c92a32d5
Reinaldo Salla <reinaldosalla95@gmail.com>
2020-06-27 11:41:52 -0300

    Crated a unidirectional sliding/opacity effect for Courosel v2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -42,7 +42,11 @@ const App = () => {
 export default App;     
 */
 
-import React from "react";
+import React, { 
+  Fragment,
+  useReducer,
+  useEffect
+} from "react";
 import js1 from "./assets/js1.png"
 import js2 from "./assets/js2.jpg";
 import js3 from "./assets/js3.png";
@@ -50,75 +54,137 @@ import js4 from "./assets/js4.jpg";
 import js5 from "./assets/js5.jpg";
 import "./App.css";
 
-const slides = [
-	{
-		imgUrl: js1,
-		className: "first-img"
-	},
-	{
-		imgUrl: js2,
-		className: "second-img"
-	},
-	{
-		imgUrl: js3,
-		className: "third-img"
-	},
-	{
-		imgUrl: js4,
-		className: "forth-img"
-	},
-	{
-		imgUrl: js5,
-		className: "fifth-img"
-	}
-];
+const duration = 4000;
 
-const activateFirstImg = (state) => {
-  return state;
+const slides = [js1, js2, js3, js4, js5];
+
+const reference = {
+  firstImgClassName: "first-img-off",
+  secondImgClassName: "second-img-off",
+  thirdImgClassName: "third-img-off",
+  forthImgClassName: "forth-img-off",
+  fifthImgClassName: "fifth-img-off"
 };
 
-const imgsSequenceClassNamesReference = [
-  "first-img-on",
-  "second-img-off",
-  "third-img-off",
-  "forth-img-off",
-  "fifth-img-off"
-];
+const initialState = {
+  ...reference,
+  firstImgClassName: "first-img-off",
+  index: 0
+};
+
+const incrementIndex = (state) => {
+  let newIndex = state.index + 1;
+  if (newIndex === 5) newIndex = 0;
+  return {
+    ...state,
+    index: newIndex
+  };
+};
+
+const moveToFirstIndex = (state) => ({
+  ...state,
+  ...reference,
+  firstImgClassName: "first-img-on"
+});
+
+const moveToSecondIndex = (state) => ({
+  ...state,
+  ...reference,
+  secondImgClassName: "second-img-on"
+});
+
+const moveToThirdIndex = (state) => ({
+  ...state,
+  ...reference,
+  thirdImgClassName: "third-img-on"
+});
+
+const moveToForthIndex = (state) => ({
+  ...state,
+  ...reference,
+  forthImgClassName: "forth-img-on"
+});
+
+const moveToFifthIndex = (state) => ({
+  ...state,
+  ...reference,
+  fifthImgClassName: "fifth-img-on"
+});
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case "activate-first-img":
-      return activateFirstImg(state);
+    case "INCREMENT_INDEX":
+      return incrementIndex(state);
+    case "MOVE_TO_FIRST_INDEX":
+      return moveToFirstIndex(state);
+    case "MOVE_TO_SECOND_INDEX":
+      return moveToSecondIndex(state);
+    case "MOVE_TO_THIRD_INDEX":
+      return moveToThirdIndex(state);
+    case "MOVE_TO_FORTH_INDEX":
+      return moveToForthIndex(state);
+    case "MOVE_TO_FIFTH_INDEX":
+      return moveToFifthIndex(state);
     default:
-      throw new Error(`Action type ${action.type} is not declared`);
+      throw new ReferenceError(`Action type ${action.type} is not declared`);
   }
 };
 
-
 const Courosel = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      dispatch({ type: "INCREMENT_INDEX" });
+    }, duration);
+    return () => clearInterval(intervalId);
+  }, [state.index]);
+
+  useEffect(() => {
+    switch (state.index) {
+      case 0:
+        dispatch({ type: "MOVE_TO_FIRST_INDEX" });
+        break;
+      case 1:
+        dispatch({ type: "MOVE_TO_SECOND_INDEX" });
+        break;
+      case 2:
+        dispatch({ type: "MOVE_TO_THIRD_INDEX" });
+        break;
+      case 3:
+        dispatch({ type: "MOVE_TO_FORTH_INDEX" });
+        break;
+      case 4:
+        dispatch({ type: "MOVE_TO_FIFTH_INDEX" });
+        break;
+      default:
+        throw new ReferenceError(`Index ${state.index} is not defined`);
+    }
+  }, [state.index]);
+
   return (
-    <div>
+    <Fragment>
       <button>go</button>
       <div className="courosel-container">
         <div className="courosel">
-          <div className="first-img">
-            <img className="courosel-img" src={slides[0].imgUrl} alt=""/>
+          <div className={state.firstImgClassName}>
+            <img className="courosel-img" src={slides[0]} alt=""/>
           </div>
-          <div className="second-img">
-            <img className="courosel-img" src={slides[1].imgUrl} alt=""/>
+          <div className={state.secondImgClassName}>
+            <img className="courosel-img" src={slides[1]} alt=""/>
           </div>
-          <div className="third-img">
-            <img className="courosel-img" src={slides[2].imgUrl} alt=""/>
+          <div className={state.thirdImgClassName}>
+            <img className="courosel-img" src={slides[2]} alt=""/>
           </div>
-          <div className="forth-img">
-            <img className="courosel-img" src={slides[3].imgUrl} alt=""/>
+          <div className={state.forthImgClassName}>
+            <img className="courosel-img" src={slides[3]} alt=""/>
           </div>
-          <div className="fifth-img">
-            <img className="courosel-img" src={slides[4].imgUrl} alt=""/>
+          <div className={state.fifthImgClassName}>
+            <img className="courosel-img" src={slides[4]} alt=""/>
           </div>
         </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
